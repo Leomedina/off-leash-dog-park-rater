4daa741074accac94067c9093c80e03763646dc0
Leo <medinayleo@gmail.com>
2021-01-04 16:32:18 -0500

    gotta say, this looks prretty good

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var express = require('express'),
   app = express(),
   path = require('path'),
   ejsMate = require('ejs-mate'),
+  morgan = require('morgan'),
   homeRoutes = require('./routes/home');
 
 /** Express Configuration */
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('ejs', ejsMate);
@@ -15,7 +17,10 @@ app.engine('ejs', ejsMate);
 /** Application Routes */
 app.use('/', homeRoutes);
 
-/** Error Handlers */
+/** 404 Handler */
+app.use(function (req, res) {
+  res.redirect('/');
+});
 
 /** Exports */
 module.exports = app;
